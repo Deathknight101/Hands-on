7a102df388b99c1d55aa3ebd894f96a690f7fb2d
Deathknight101 <johnandy969@gmail.com>
2024-05-13 22:00:09 +0600

    Added user conditionals renders to navbar.

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,12 +1,14 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const path = useLocation().pathname;
+  const navigate = useNavigate();
+  const user = true;
   const textStrokeStyle = {
     WebkitTextStroke: "0.5px white",
     textStroke: "0.5px white",
     color: "transparent",
   };
-  const path = useLocation().pathname;
 
   const links = (
     <>
@@ -49,7 +51,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="dropdown-content mt-2 z-[1] p-2 shadow bg-background border-primary border text-text w-52"
+              className="dropdown-content mt-2 z-[50] p-2 shadow bg-background border-primary border text-text w-52"
             >
               {links}
             </ul>
@@ -64,9 +66,60 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <button className="btn bg-transparent hover:bg-white text-white hover:text-primary border-white hover:border-white">
-            SIGN IN
-          </button>
+          {user ? (
+            <>
+              <div className="flex items-center gap-2">
+                <div className="dropdown dropdown-end">
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    className="tooltip flex justify-center items-center  rounded-full tooltip-left btn btn-circle"
+                    data-tip="displayName"
+                  >
+                    <div className="w-12 rounded-full">
+                      <img
+                        className="rounded-full"
+                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      />
+                    </div>
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content mt-2 z-[50] p-2 shadow bg-background border-primary border text-text w-52 flex flex-col gap-y-1 font-bold"
+                  >
+                    <Link
+                      to={"/"}
+                      className="btn bg-primary text-background hover:bg-primary hover:text-background border-none rounded-none w-full"
+                    >
+                      Manage My Posts
+                    </Link>
+                    <hr />
+                    <Link
+                      to={"/"}
+                      className="btn bg-primary text-background hover:bg-primary hover:text-background border-none rounded-none w-full"
+                    >
+                      Add Volunteer Post
+                    </Link>
+                  </ul>
+                </div>
+                <button
+                  className="btn bg-transparent hover:bg-white text-white hover:text-primary border-white hover:border-white rounded-none"
+                  onClick={() => navigate("/login")}
+                >
+                  SIGN OUT
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <button
+                className="btn bg-transparent hover:bg-white text-white hover:text-primary border-white hover:border-white"
+                onClick={() => navigate("/login")}
+              >
+                SIGN IN
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
